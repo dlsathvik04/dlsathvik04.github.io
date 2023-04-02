5c863e3d4222ab01717a1522f97b4530aed4a3c7
Lekha Sathvik Devabathini <dlsathvik04@gmail.com>
2023-04-02 10:32:02 +0530

    tried to fix missing image

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,9 +26,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "921c70bb8ea458ebdb5d0fe2c330c2b9",
-"/": "921c70bb8ea458ebdb5d0fe2c330c2b9",
-"main.dart.js": "7c3e1e6ab34e010976cc687fa3fd300f",
+"index.html": "c719a614b3031097eda16aa66bc222e9",
+"/": "c719a614b3031097eda16aa66bc222e9",
+"main.dart.js": "95396baa7c2b23084ce1003a34bcb501",
 "manifest.json": "e74af8957b5899dc6da961caee768ec9",
 "version.json": "009c9e65172e010890f7f65fde438006"
 };
